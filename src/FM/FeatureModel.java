f48930785c8293bfb1040fa9e5d215fdc4f13e4e
juancruz <juancruz@juan>
2017-07-31 23:54:26 -0300

    change method to print tree

---
+++ src/FM/FeatureModel.java
@@ -0,0 +1,61 @@
+package FM;
+
+/**
+ * Variability encoding of the feature model for KeY.
+ * Auto-generated class.
+ */
+public class FeatureModel {
+
+	public static boolean beautiful;
+	public static boolean hello;
+	public static boolean wonderful;
+	public static boolean guys;
+	public static boolean world;
+	public static boolean feature;
+	public static boolean helloworld;
+	
+	public  boolean isBeautiful() {
+		return beautiful;
+	}
+	public  void setBeautiful(boolean beautiful) {
+		FeatureModel.beautiful = beautiful;
+	}
+	public  boolean isHello() {
+		return hello;
+	}
+	public  void setHello(boolean hello) {
+		FeatureModel.hello = hello;
+	}
+	public  boolean isWonderful() {
+		return wonderful;
+	}
+	public  void setWonderful(boolean wonderful) {
+		FeatureModel.wonderful = wonderful;
+	}
+	public  boolean isGuys() {
+		return guys;
+	}
+	public  void setGuys(boolean guys) {
+		FeatureModel.guys = guys;
+	}
+	public  boolean isWorld() {
+		return world;
+	}
+	public void setWorld(boolean world) {
+		FeatureModel.world = world;
+	}
+	public  boolean isFeature() {
+		return feature;
+	}
+	public  void setFeature(boolean feature) {
+		FeatureModel.feature = feature;
+	}
+	public  boolean isHelloworld() {
+		return helloworld;
+	}
+	public  void setHelloworld(boolean helloworld) {
+		FeatureModel.helloworld = helloworld;
+	}
+
+	
+}
