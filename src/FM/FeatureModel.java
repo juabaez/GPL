e80ca567d731a57c039db39f50efb7ab76fa7965
juancruz <juancruz@juan>
2017-08-01 20:50:14 -0300

    generate random tree

--- src/FM/FeatureModel.java
+++ src/FM/FeatureModel.java
@@ -1,18 +1,20 @@
 package FM;
 
+import java.util.Random;
+
 /**
  * Variability encoding of the feature model for KeY.
  * Auto-generated class.
  */
 public class FeatureModel {
 
-	public static boolean beautiful;
-	public static boolean hello;
-	public static boolean wonderful;
-	public static boolean guys;
-	public static boolean world;
-	public static boolean feature;
-	public static boolean helloworld;
+	private static boolean beautiful;
+	private static boolean hello;
+	private static boolean wonderful;
+	private static boolean guys;
+	private static boolean world;
+	private static boolean feature;
+	private static boolean helloworld;
 	
 	public  boolean isBeautiful() {
 		return beautiful;
@@ -57,5 +59,68 @@ public class FeatureModel {
 		FeatureModel.helloworld = helloworld;
 	}
 
+	public FeatureModel(){
+		Random r = new Random();
+		FeatureModel.beautiful = r.nextBoolean();
+		FeatureModel.hello = r.nextBoolean();
+		FeatureModel.wonderful = r.nextBoolean();
+		FeatureModel.guys = r.nextBoolean();
+		FeatureModel.world = r.nextBoolean();
+		FeatureModel.feature = r.nextBoolean();
+		FeatureModel.helloworld = r.nextBoolean();
+	}
 	
+	/*@  @*/
+	 private void  print__wrappee__Hello  (){
+	      System.out.print("Hello");
+	   }
+
+	/*@ 
+	  @ requires FM.FeatureModel.helloworld && (FM.FeatureModel.hello || !guys) && (FM.FeatureModel.feature || !beautiful) && (FM.FeatureModel.feature || !wonderful) && (FM.FeatureModel.beautiful || FM.FeatureModel.wonderful || !feature) && (!beautiful || !wonderful) && (FM.FeatureModel.helloworld || !hello) && (FM.FeatureModel.helloworld || !feature) && (FM.FeatureModel.helloworld || !world) && (FM.FeatureModel.hello || !helloworld) && (FM.FeatureModel.world || !helloworld) && true && !false;
+	  @ requires FM.FeatureModel.hello || FM.FeatureModel.guys;
+	  @*/
+	 private void  print__wrappee__Guys  () throws Exception {
+		if (!FM.FeatureModel.guys) {
+			print__wrappee__Hello();
+			return;
+			//throw new Exception("guys is false ");
+		}
+     print__wrappee__Hello();
+     System.out.print(" Guys!");
+  }
+
+	/*@ 
+	  @ requires FM.FeatureModel.helloworld && (FM.FeatureModel.hello || !guys) && (FM.FeatureModel.feature || !beautiful) && (FM.FeatureModel.feature || !wonderful) && (FM.FeatureModel.beautiful || FM.FeatureModel.wonderful || !feature) && (!beautiful || !wonderful) && (FM.FeatureModel.helloworld || !hello) && (FM.FeatureModel.helloworld || !feature) && (FM.FeatureModel.helloworld || !world) && (FM.FeatureModel.hello || !helloworld) && (FM.FeatureModel.world || !helloworld) && true && !false;
+	  @ requires FM.FeatureModel.hello || FM.FeatureModel.guys || FM.FeatureModel.beautiful;
+	  @*/
+	 private void  print__wrappee__Beautiful  () throws Exception {
+		if (!FM.FeatureModel.beautiful) {
+			print__wrappee__Guys();
+			return;
+			//throw new Exception("beautiful is false ");
+		}
+     print__wrappee__Guys();
+     System.out.print(" Beautiful!");
+  }
+
+	/*@ 
+	  @ requires FM.FeatureModel.helloworld && (FM.FeatureModel.hello || !guys) && (FM.FeatureModel.feature || !beautiful) && (FM.FeatureModel.feature || !wonderful) && (FM.FeatureModel.beautiful || FM.FeatureModel.wonderful || !feature) && (!beautiful || !wonderful) && (FM.FeatureModel.helloworld || !hello) && (FM.FeatureModel.helloworld || !feature) && (FM.FeatureModel.helloworld || !world) && (FM.FeatureModel.hello || !helloworld) && (FM.FeatureModel.world || !helloworld) && true && !false;
+	  @ requires FM.FeatureModel.hello || FM.FeatureModel.guys || FM.FeatureModel.beautiful || FM.FeatureModel.wonderful;
+	  @*/
+	 private void  print__wrappee__Wonderful  () throws Exception {
+		if (!FM.FeatureModel.wonderful) {
+			print__wrappee__Beautiful();
+			return;
+			//throw new Exception("Wonderful is false ");
+		}
+     print__wrappee__Beautiful();
+     System.out.print(" Wonderful!");
+  }
+
+	/*@  @*/
+	public void print() throws Exception {
+		print__wrappee__Wonderful();
+		System.out.print(" world!");
+  }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,12 @@
 import FM.FeatureModel;
 
 public  class Main {
-	/*@  @*/
-	 private void  print__wrappee__Hello  (){
-	      System.out.print("Hello");
-	   }
-
-	/*@ 
-	  @ requires FM.FeatureModel.helloworld && (FM.FeatureModel.hello || !guys) && (FM.FeatureModel.feature || !beautiful) && (FM.FeatureModel.feature || !wonderful) && (FM.FeatureModel.beautiful || FM.FeatureModel.wonderful || !feature) && (!beautiful || !wonderful) && (FM.FeatureModel.helloworld || !hello) && (FM.FeatureModel.helloworld || !feature) && (FM.FeatureModel.helloworld || !world) && (FM.FeatureModel.hello || !helloworld) && (FM.FeatureModel.world || !helloworld) && true && !false;
-	  @ requires FM.FeatureModel.hello || FM.FeatureModel.guys;
-	  @*/
-	 private void  print__wrappee__Guys  () throws Exception {
-		if (!FM.FeatureModel.guys) {
-			print__wrappee__Hello();
-			return;
-			//throw new Exception("guys is false ");
-		}
-      print__wrappee__Hello();
-      System.out.print(" Guys!");
-   }
-
-	/*@ 
-	  @ requires FM.FeatureModel.helloworld && (FM.FeatureModel.hello || !guys) && (FM.FeatureModel.feature || !beautiful) && (FM.FeatureModel.feature || !wonderful) && (FM.FeatureModel.beautiful || FM.FeatureModel.wonderful || !feature) && (!beautiful || !wonderful) && (FM.FeatureModel.helloworld || !hello) && (FM.FeatureModel.helloworld || !feature) && (FM.FeatureModel.helloworld || !world) && (FM.FeatureModel.hello || !helloworld) && (FM.FeatureModel.world || !helloworld) && true && !false;
-	  @ requires FM.FeatureModel.hello || FM.FeatureModel.guys || FM.FeatureModel.beautiful;
-	  @*/
-	 private void  print__wrappee__Beautiful  () throws Exception {
-		if (!FM.FeatureModel.beautiful) {
-			print__wrappee__Guys();
-			return;
-			//throw new Exception("beautiful is false ");
-		}
-      print__wrappee__Guys();
-      System.out.print(" Beautiful!");
-   }
-
-	/*@ 
-	  @ requires FM.FeatureModel.helloworld && (FM.FeatureModel.hello || !guys) && (FM.FeatureModel.feature || !beautiful) && (FM.FeatureModel.feature || !wonderful) && (FM.FeatureModel.beautiful || FM.FeatureModel.wonderful || !feature) && (!beautiful || !wonderful) && (FM.FeatureModel.helloworld || !hello) && (FM.FeatureModel.helloworld || !feature) && (FM.FeatureModel.helloworld || !world) && (FM.FeatureModel.hello || !helloworld) && (FM.FeatureModel.world || !helloworld) && true && !false;
-	  @ requires FM.FeatureModel.hello || FM.FeatureModel.guys || FM.FeatureModel.beautiful || FM.FeatureModel.wonderful;
-	  @*/
-	 private void  print__wrappee__Wonderful  () throws Exception {
-		if (!FM.FeatureModel.wonderful) {
-			print__wrappee__Beautiful();
-			return;
-			//throw new Exception("Wonderful is false ");
-		}
-      print__wrappee__Beautiful();
-      System.out.print(" Wonderful!");
-   }
-
-	/*@  @*/
-	public void print() throws Exception {
-		print__wrappee__Wonderful();
-		System.out.print(" world!");
-   }
 
 	/*@  @*/
 	public static void main(String[] args) {
 		FeatureModel f = new FeatureModel();
-		f.setWorld(true);
 	      try {
-			new Main().print();
+			f.print();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
